1b2c778a4848ca718fbbb1eba0e4abf8a1234a0d
Sumit Sheoran <sumitsheoran4264@gmail.com>
2026-05-09 21:06:54 +0530

    Name update test

--- Tip_Calculator.py
+++ Tip_Calculator.py
@@ -5,7 +5,7 @@ people = int(input("How many people to split the bill? "))
 total = bill + (bill * tip / 100)
 per_person = total / people #This line calculates the amount each person should pay by dividing the total bill (including tip) by the number of people splitting the bill. The result is stored in the variable per_person.
 print(f"Each person should pay: ${per_person:.2f}") #The :.2f format specifier formats the per_person value to 2 decimal places, ensuring that the output is displayed as a monetary amount with two digits after the decimal point. 
-
+#end of code
 
 
 
